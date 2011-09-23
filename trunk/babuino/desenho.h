932094b7dfdd447067ad89c28491a3b58308435c
andreesteve <andreesteve@b73b2442-35dc-f789-64ad-9286c533472d>
2011-09-23 13:53:47 +0000

    Threads parecem funcionar. Inicio da implementacao do desenho

---
+++ trunk/babuino/desenho.h
@@ -0,0 +1,38 @@
+#ifndef __DESENHO__
+#define __DESENHO__
+
+#include <curses.h>
+#include <string.h>
+
+typedef struct mapa
+{
+   int ponte_x, ponte_y, ponte_len;
+} mapa_t;
+
+
+void desenho_init();
+
+void desenho_destroy();
+
+void desenho_mapa();
+
+/* cria novo macaco e retorna id do macaco
+ tambem desenha macaco no lado certo */
+int desenho_novo_macaco(int sentido);
+
+/* muda sentido da corda */
+void desenho_muda_corda(int novo_sentido, int id_macaco);
+
+/* informa que macaco entrou na corda (desenho_tenta_corda deve ser chamado antes) */
+void desenho_entra_corda(int id_macaco);
+
+/* informa que macaco saiu da corda */
+void desenho_sai_corda(int id_macaco);
+
+/* informa que macaco tentou pegar a corda */
+void desenho_tenta_corda(int id_macaco);
+
+/* informa que macaco desistiu da corda */
+void desenho_desiste_corda(int id_macaco);
+
+#endif
