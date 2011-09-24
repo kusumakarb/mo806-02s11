6b170564f9c272f0857b23a8b547dd9d062c38f2
andreesteve <andreesteve@b73b2442-35dc-f789-64ad-9286c533472d>
2011-09-24 22:10:16 +0000

    Macacao desenhado

--- trunk/babuino/desenho.c
+++ trunk/babuino/desenho.c
@@ -1,37 +1,87 @@
 #include "desenho.h"
 
-mapa_t m;
+#define _COR(C,X) attron(COLOR_PAIR(C)); X attroff(COLOR_PAIR(C));
+#define BRANCO(X) _COR(1,X)
+#define BRANCO(X) _COR(1,X)
+#define BRANCO(X) _COR(1,X)
+
+#define LOCK(X) sem_wait(&lock); X sem_post(&lock);
+
+mapa_t mapa;
+sem_t lock;
+
+volatile macaco_t* macacos;
+
+void desenha_macaco_grde(int x, int y)
+{
+/*
+         .-"-.
+       _/.-.-.\_
+      ( ( o o ) )
+       |/  "  \|
+        \  O  /
+        /`"""`\
+       /       \
+*/
+
+   mvprintw(y-6, x, "   .-\"-.");
+   mvprintw(y-5, x, " _/.-.-.\\_");
+   mvprintw(y-4, x, "( ( o o ) )");
+   mvprintw(y-3, x, " |/  \"  \\|");
+   mvprintw(y-2, x, "  \\  O  /");
+   mvprintw(y-1, x, "  /`\"\"\"`\\");
+   mvprintw(y-0, x, " /       \\");
+}
+
+void desenha_macaco(macaco_t* m)
+{
+   char* d;
+
+   if (m->sentido == 1)
+      d = ">";
+   else
+      d = "<";
+
+   mvprintw(m->y, m->x, d);
+}
 
 void desenha_mapa()
 {
    int col, row;
    int i, j;
+   mapa_t* m;
    int distancia_topo = 10;
    char* ponte = "--------------------------------------------------------";
 
+   m = &mapa;
+
    getmaxyx(stdscr,row,col);
 
-   m.ponte_x = ( col - strlen(ponte) ) /2;
-   m.ponte_y = distancia_topo;
-   m.ponte_len = strlen(ponte);
+   m->ponte_x = ( col - strlen(ponte) ) /2;
+   m->ponte_y = distancia_topo;
+   m->ponte_len = strlen(ponte);
 
    // ponte
-   mvprintw(m.ponte_y, m.ponte_x, ponte);
+   mvprintw(m->ponte_y, m->ponte_x, ponte);
 
    // altura
-   for (i = m.ponte_y + 1; i < row; i++)
+   for (i = m->ponte_y + 1; i < row; i++)
    {
-      mvprintw(i, m.ponte_x, "|");
-      mvprintw(i,m.ponte_x + m.ponte_len - 1, "|");
+      mvprintw(i, m->ponte_x, "|");
+      mvprintw(i, m->ponte_x + m->ponte_len - 1, "|");
    }
 
    // chao esq
-   for (i = m.ponte_x - 1; i >= 0; i--)
-      mvprintw(m.ponte_y, i, "_");
+   for (i = m->ponte_x - 1; i >= 0; i--)
+      mvprintw(m->ponte_y, i, "_");
 
    // chao dir
-   for (i = m.ponte_x + m.ponte_len + 1; i < col; i++)
-      mvprintw(m.ponte_y, i, "_");
+   for (i = m->ponte_x + m->ponte_len + 1; i < col; i++)
+      mvprintw(m->ponte_y, i, "_");
+
+   // macaco
+
+   desenha_macaco_grde(m->ponte_x, 6);
 
 }
 
@@ -40,17 +90,80 @@ void desenho_init()
    // init ncurses
    initscr();
 
+   // init lock
+   sem_init(&lock, 0, 1);
+
    // invisible cursor
    curs_set(0);
+
+   start_color();
+
+   init_pair(1, COLOR_WHITE, COLOR_BLACK);
+
+   macacos = NULL;
 
    desenha_mapa();
 }
 
 void desenho_destroy()
 {
+   // destroy lock
+   sem_destroy(&lock);
+
    // wait for user to finish
    getch();
 
    // finish ncurses
    endwin();
+
+   for (; macacos != NULL; macacos = macacos = macacos->next)
+      free((void*)macacos);
+
+   macacos = NULL;
+}
+
+macaco_t* new_macaco(int sentido)
+{
+   int i;
+   macaco_t* m, *n;
+   i = 0;
+
+   m = (macaco_t*)malloc(sizeof(macaco_t));
+   m->sentido = sentido;
+   m->next = NULL;
+
+   LOCK
+   (
+      if (macacos == NULL)
+         macacos = m;
+      else
+      {
+         for (n = (macaco_t*)macacos; n->next != NULL; n = n->next)
+            i++;
+
+         n->next = m;
+      }
+
+      m->id = i;
+   )
+
+   return m;
+}
+
+macaco_t* get_macaco(int id)
+{
+   macaco_t* m;
+
+   LOCK
+   (
+      m = (macaco_t*)macacos;
+      while (id--) m = m->next;
+   )
+
+   return m;
+}
+
+int desenho_novo_macaco(int sentido)
+{
+   new_macaco(sentido);
 }
